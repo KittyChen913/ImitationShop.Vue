89cdf8687e4b4c03d259fa90814ee653f2ac8ac7
KittyChen913 <nightmare.kitty111@gmail.com>
2022-05-09 01:11:45 +0800

    ✨ 設計主頁面基礎 Layout
    導入 Ant Design Vue

--- imitationshop.vue/src/App.vue
+++ imitationshop.vue/src/App.vue
@@ -1,24 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <a-layout>
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+      <div class="logo">
+        <img alt="Vue logo" src="./assets/logo.png" width="50" />
+      </div>
+    </a-layout-header>
+    <a-layout-content :style="{ padding: '50px 50px', marginTop: '64px' }">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '400px' }">
+        Content
+      </div>
+    </a-layout-content>
+    <a-layout-footer :style="{ textAlign: 'center' }">
+      Ant Design ©2022 Created by Kitty.Chen
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-  }
-});
+  components: {},
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#components-layout-demo-fixed .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 24px 16px 0;
+  float: left;
+}
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+[data-theme='dark'] .site-layout .site-layout-background {
+  background: #141414;
 }
 </style>
