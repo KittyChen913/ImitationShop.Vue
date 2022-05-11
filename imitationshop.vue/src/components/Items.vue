23940b86e875d16b0ee8af58c2827839c520db33
KittyChen913 <nightmare.kitty111@gmail.com>
2022-05-11 22:28:01 +0800

    ✨ Add 商品清單
    導入 vuex

--- imitationshop.vue/src/components/Items.vue
+++ imitationshop.vue/src/components/Items.vue
@@ -1,15 +1,47 @@
 <template>
   <h2>商品列表</h2>
+  <div>{{ greeting }}</div>
+  <a-row :gutter="8">
+    <a-col
+      :xs="10"
+      :sm="8"
+      :md="6"
+      :lg="4"
+      v-for="item in items"
+      :key="item.itemId"
+    >
+      <a-card hoverable>
+        <template #cover>
+          <img
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        </template>
+        <a-card-meta v-bind:title="item.itemName">
+          <template #description>NT${{ item.itemPrice }}</template>
+        </a-card-meta>
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { mapActions, mapState } from 'vuex'
 
 export default defineComponent({
   name: 'Items-page',
+  computed: {
+    ...mapState(['items']),
+  },
+  methods: {
+    ...mapActions(['fetchItemList']),
+  },
+  created() {
+    this.fetchItemList({ id: '78' })
+  },
 })
 </script>
 
 <style scoped>
-
 </style>
---
+++ imitationshop.vue/src/store/index.ts
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    items: {}
+  },
+  getters: {
+  },
+  mutations: {
+    setItems(state, payload) {
+      state.items = payload
+    }
+  },
+  actions: {
+    fetchItemList(context, payload) {
+      fetch('https://localhost:7227/api/Items')
+        .then(response => response.json())
+        .then(data => context.commit('setItems', data))
+    }
+  },
+  modules: {
+  }
+})
