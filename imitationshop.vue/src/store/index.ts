5cd7487c5c486216fb71f971e60d85570766936c
KittyChen913 <nightmare.kitty111@gmail.com>
2022-05-13 22:10:50 +0800

    ✨ Add 商品詳情內容

--- imitationshop.vue/src/store/index.ts
+++ imitationshop.vue/src/store/index.ts
@@ -2,13 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    items: {}
+    items: {},
+    itemDetail: {}
   },
   getters: {
   },
   mutations: {
     setItems(state, payload) {
       state.items = payload
+    },
+    setItemDetail(state, payload) {
+      state.itemDetail = payload
     }
   },
   actions: {
@@ -16,6 +20,11 @@ export default createStore({
       fetch('https://localhost:7227/api/Items')
         .then(response => response.json())
         .then(data => context.commit('setItems', data))
+    },
+    fetchItemDetail(context, payload) {
+      fetch('https://localhost:7227/api/Items/' + payload)
+        .then(response => response.json())
+        .then(data => context.commit('setItemDetail', data))
     }
   },
   modules: {
--- imitationshop.vue/src/views/ItemDetail.vue
+++ imitationshop.vue/src/views/ItemDetail.vue
@@ -1,13 +1,61 @@
 <template>
   <h2>商品詳情頁</h2>
   <h3>{{ $route.params.itemId }}</h3>
+  <a-row justify="center">
+    <a-col :span="12">
+      <a-image-preview-group>
+        <a-row justify="center">
+          <a-col>
+            <a-image :width="200" src="https://aliyuncdn.antdv.com/vue.png" />
+          </a-col>
+        </a-row>
+        <a-row justify="center">
+          <a-col>
+            <a-image :width="50" src="https://aliyuncdn.antdv.com/logo.png" />
+            <a-image :width="50" src="https://aliyuncdn.antdv.com/vue.png" />
+            <a-image :width="50" src="https://aliyuncdn.antdv.com/logo.png" />
+            <a-image :width="50" src="https://aliyuncdn.antdv.com/vue.png" />
+          </a-col>
+        </a-row>
+      </a-image-preview-group>
+    </a-col>
+    <a-col :span="12">
+      <a-form>
+        <a-form-item>
+          <h2 class="ant-form-text">{{ itemDetail.ItemName }}</h2>
+        </a-form-item>
+        <a-form-item label="價格">
+          <span class="ant-form-text">{{ itemDetail.Price }}</span>
+        </a-form-item>
+        <a-form-item label="數量">
+          <span class="ant-form-text">{{ itemDetail.Amount }}</span>
+        </a-form-item>
+        <a-form-item label="上架時間">
+          <span class="ant-form-text">{{ itemDetail.CreateDate }}</span>
+        </a-form-item>
+        <a-form-item label="商品詳情">
+          <span class="ant-form-text">{{ itemDetail.Description }}</span>
+        </a-form-item>
+      </a-form>
+    </a-col>
+  </a-row>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { mapState, mapActions } from 'vuex'
 
 export default defineComponent({
-  name: 'ItemDetail',
+  name: 'ItemDetail-page',
+  computed: {
+    ...mapState(['itemDetail']),
+  },
+  methods: {
+    ...mapActions(['fetchItemDetail']),
+  },
+  created() {
+    this.fetchItemDetail(this.$route.params.itemId)
+  },
 })
 </script>
 
